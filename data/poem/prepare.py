fb3195ee23eb78c7197eb58829459e356192579a
hossein pirhadi <hp@DESKTOP-FMTDPN3>
2024-07-26 18:34:16 +0330

    fix dataset

--- data/poem/prepare.py
+++ data/poem/prepare.py
@@ -3,7 +3,7 @@ import tiktoken
 import numpy as np
 import pandas as pd
 
-df = pd.read_csv('data/poem/khayyam.txt')
+df = pd.read_csv('./data/poem/poem.txt')
 data = df['body'].str.cat(sep='\n')
 
 n = len(data)
@@ -11,7 +11,27 @@ train_data = data[:int(n*0.9)]
 val_data = data[int(n*0.9):]
 
 # encode with tiktoken gpt2 bpe
-enc = tiktoken.get_encoding("gpt2")
+gpt2_base = tiktoken.get_encoding("gpt2")
+
+# In production, load the arguments directly instead of accessing private attributes
+# See openai_public.py for examples of arguments for specific encodings
+enc = tiktoken.Encoding(
+    # If you're changing the set of special tokens, make sure to use a different name
+    # It should be clear from the name what behaviour to expect.
+    name="gpt2_im",
+    pat_str=gpt2_base._pat_str,
+    mergeable_ranks=gpt2_base._mergeable_ranks,
+    special_tokens={
+        **gpt2_base._special_tokens,
+        "<|im_start|>": 50257,
+        "<|im_end|>": 50258,
+        "<|startofstanza|>": 50259,
+        "<|startofverse|>": 50260,
+        "<|endofverse|>": 50261,
+
+    }
+)
+
 train_ids = enc.encode_ordinary(train_data)
 val_ids = enc.encode_ordinary(val_data)
 print(f"train has {len(train_ids):,} tokens")
